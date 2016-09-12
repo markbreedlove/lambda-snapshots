d5584e4b82cdb83d56ea1169e870e4ee30e0b1a3
Mark Breedlove <mb@markbreedlove.com>
2016-09-11 22:04:55 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lambda Snapshots
+
+Python module with AWS Lambda functions to make EBS volume snapshots and
+delete old ones.
+
+See http://docs.aws.amazon.com/lambda/latest/dg/lambda-python-how-to-create-deployment-package.html
+
+To create `.zip` file for uploading:
+```
+$ cd lambda-snapshots
+$ zip -r lambda-snapshots.zip .
+```
+Then upload that zipfile when you create your Lambda function.
+
+Create separate Lambda functions for backup and delete, but upload the same
+zipfile. When you create your function, use the appropriate function name
+for the `Handler` property.  For example, I have a Lambda function named
+`make_snapshots` and its Handler is `snapshots.make_snapshots` because
+`snapshots.py` is the `snapshots` module and `make_snapshots` is the function
+that I want.
---
+++ snapshots.py
@@ -0,0 +1,77 @@
+"""AWS Lambda functions for EBS Volume Snapshots"""
+
+import boto3
+from datetime import tzinfo, timedelta, datetime
+
+DAYS_TO_KEEP = 30
+
+class UTC(tzinfo):
+    """
+    A `tzinfo' derived class for representing UTC.
+
+    Necessary for getting an offset-aware `datetime' object to represent "now"
+    in order to calculate date deltas.
+
+    There may be classes that make it more convenient to determine these
+    deltas, but I am trying not to include dependencies unless I have to.
+    """
+    def __init__(self, *args, **kwargs):
+        self.__zero_delta = timedelta(0)
+        super(UTC, self).__init__(*args, **kwargs)
+    def utcoffset(self, dt):
+        return self.__zero_delta
+    def tzname(self, dt):
+        return 'UTC'
+    def dst(self, dt):
+        return self.__zero_delta
+
+def make_snapshots(event, context):
+    """Make EBS snapshots from all volumes tagged with "Backup" == "true" """
+    client = boto3.client('ec2')
+    vols = client.describe_volumes()['Volumes']
+    vols_to_do = (v for v in vols if has_backup_tag(v))
+    for v in vols_to_do:
+        client.create_snapshot(
+            VolumeId=v['VolumeId'],
+            Description=volume_desc(v)
+        )
+
+def delete_old_snapshots(event, context):
+    """Delete old snapshots older than DAYS_TO_KEEP days
+
+    It may be worthwhile to have the days parameter optionally come from
+    a JSON Constant, declared in the "inputs" section of a Rule in a CloudWatch
+    scheduled event.
+    """
+    account = event['account']
+    client = boto3.client('ec2')
+    snaps = client.describe_snapshots(OwnerIds=[account])['Snapshots']
+    old_snaps = (s for s in snaps if older_than(s['StartTime'], DAYS_TO_KEEP))
+    for s in old_snaps:
+        client.delete_snapshot(SnapshotId=s['SnapshotId'])
+
+def older_than(startdate, days):
+    nowdate = datetime.now(UTC())
+    return (nowdate - startdate).days > days
+
+def has_backup_tag(volume):
+    tags = volume.get('Tags', [])
+    for t in tags:
+        if t['Key'] == 'Backup' and t['Value'] == 'true':
+            return True
+    return False
+
+def volume_desc(volume):
+    tags = volume.get('Tags', [])
+    name = name_tag(tags)
+    if name:
+        return "Backup of %s" % name
+    else:
+        return "Backup of %s" % volume['VolumeId']
+
+def name_tag(tags):
+    if tags:
+        for t in tags:
+            if t['Key'] == 'Name':
+                return t['Value']
+    return ''
